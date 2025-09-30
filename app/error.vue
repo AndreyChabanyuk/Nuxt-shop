844e7a9a540e53075563994f4d6ed8082b797743
andron <andrejjchabanjuk@rambler.ru>
2025-09-30 21:45:50 +0300

    refactor: add error page

---
+++ app/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{ error.statusCode }}{{ error.statusMessage }}</h1>
+    </div>
+</template>
+<script lang="ts" setup>
+import type { NuxtError } from '#app'
+
+
+const { error } = defineProps<{
+    error: NuxtError
+}>()
+
+</script>
+<style lang="">
+    
+</style>
