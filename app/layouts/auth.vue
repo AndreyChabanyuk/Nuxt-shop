82de4da27cafdc43ccb6b7bd0cadbef7f8ac0da3
andron <andrejjchabanjuk@rambler.ru>
2025-10-13 09:39:03 +0300

    5-layout: start layout lesson

---
+++ app/layouts/auth.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>Auth</div>
+        <button @click="backToDefault">Обратно</button>
+        <slot />
+    </div>
+</template>
+<script setup lang="ts">
+const backToDefault = () => {
+    setPageLayout('default')
+}
+</script>
+<style lang="">
+    
+</style>
